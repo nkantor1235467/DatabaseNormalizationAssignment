aa1252f42b1265b3264004561e8288046be5f49d
nkantor1235467 <nkantor@clarku.edu>
2024-02-12 17:28:22 -0500

    This commit corrects the error in the previous comment where the markdown and sql weren't saved

--- DatabaseNormalization.sql
+++ DatabaseNormalization.sql
@@ -1,1 +1,32 @@
---This file contains the SQL code 
+--SPATIAL DATABASES LAB 2: DATABASE NORMALIZATION
+
+--Part 1: Creating a non-normalized table and normalizing to 1NF
+
+--Part 1a: Creating a Non-Normalized Table
+
+--This code creates a table named Parks_Info
+CREATE TABLE Parks_Info (
+    --creates a primary key column called ID
+    ID SERIAL PRIMARY KEY,
+    --creates a column of the character type called ParkName
+    ParkName VARCHAR(255),
+    --creates a column of the character type called Facilities
+    Facilities VARCHAR(255)
+);
+
+--This command fills the columns ParkName and Facilities with single phrases for ParkName and lists of phrases for Facilities
+INSERT INTO Parks_Info (ParkName, Facilities) VALUES
+('Central Park', 'Playground, Restroom, Picnic area'),
+('Liberty Park', 'Restroom, Picnic area'),
+('Riverside Park', 'Playground, Bike Path');
+
+--Part 1b: Normalizing the table to 1NF
+
+--This command creates a table with 2 columns, a primary key column called ParkID and a column of the character type called ParkName
+CREATE TABLE Parks (
+    --creates a primary key column called ParkID
+    ParkID SERIAL PRIMARY KEY
+    --creates a column of character type called ParkName
+    ParkName VARCHAR(255)
+);
+
--- Report.md
+++ Report.md
@@ -1,1 +1,11 @@
-## Assignment 2 Report
+# Assignment 2 Report
+
+## Part 1: Creating a Non-Normalized Table and Normalizing to 1NF
+
+### Part 1a: Creating a Non-Normalized Table
+In this section, I created a table with three columns, a primary key column, and columns titled ParkName and Facilities. The ParkName column contains the names of parks, and the Facilities column contains the types of facilities located in each park. This table is not normalized to the first normal form because the Facilities column contains divisible values rather than atomic indivisible values. For example, the facilities for Central Park are multiple: playground, restroom, and picnic area.
+
+Here is the screenshot showing the table created in pgadmin ![This image shows the PGadmin GUI. In the bottom panel is the non-normalized table and in the panel above is the query used to create it.](/Part1a.png)
+### Part 1b: Normalizing the Table to First Normal Form
+
+## Part 2:
